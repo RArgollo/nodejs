fa7fdcd4dc891891958e68d2058116a818d5c722
RArgollo <rafaelargollo22@hotmail.com>
2024-11-29 12:12:33 -0300

    conectando banco de dados

--- index.js
+++ index.js
@@ -1,8 +1,30 @@
-var http = require('http')
+import express from 'express'
+import { sequelize, Usuario } from 'db.js'
 
-http.createServer(function (req, res) {
-    res.write('BOM DIA')
-    res.end()
-}).listen(80)
+const app = express()
 
-console.log("rodou")
+const route = Router()
+
+app.use(express.json())
+
+app.use(route)
+
+app.get("/teste/:nome/:senha", async (req, res) => {
+    try {
+        await sequelize.authenticate();
+        console.log("Conexão estabelecida com o banco de dados")
+        const novoUsuario = await Usuario.create({
+            nome: req.query.nome,
+            senha: req.query.senha,
+        });
+        res.send("USUARIO INSERIDO", novoUsuario.toJSON())
+    } catch (error) {
+        res.send("Erro ao conectar/inserir no banco de dados: ", error)
+    } finally {
+        sequelize.close()
+    }
+})
+
+
+
+app.listen(3001, () => console.log("server running"))
